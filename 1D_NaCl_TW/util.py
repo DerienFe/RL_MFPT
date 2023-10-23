Fixed 1D_NaCl by applying same normalization procedure to the DHAM and bias_M_1D(). also fixed the bias_M_1D() by adding a factor of 2 to the bias potential, changed exp(u/2kT) to exp(-u/kT) in the bias potential. now the bias is placed at the correct place and the DHAM is working as expected. Some occasion with prop_step =1000, the system refuse to reach 7A although passed the energy barrier ~4A. still need double check this.

1D_NaCl_TW/util.py
@@ -33,8 +33,8 @@ def kemeny_constant_check(N, mfpt, peq):
     for i in range(N):
         for j in range(N):
             kemeny[i] = kemeny[i] + mfpt[i, j] * peq[j]
-    #print("Performing Kemeny constant check...")
-    #print("the min/max of the Kemeny constant is:", np.min(kemeny), np.max(kemeny))
+    print("Performing Kemeny constant check...")
+    print("the min/max of the Kemeny constant is:", np.min(kemeny), np.max(kemeny))
     """
     if np.max(kemeny) - np.min(kemeny) > 1e-6:
         print("Kemeny constant check failed!")
@@ -103,18 +103,16 @@ def compute_free_energy(K, kT=0.5981):
     evalues_sorted = evalues[index] #sort the eigenvalues based on index
 
     #calculate the equilibrium distribution
-    peq = evectors[:, index[-1]].T/np.sum(evectors[:, index[-1]]) #normalize the eigenvector
-    #take the real part of the eigenvector i.e. the probability distribution at equilibrium.
-    #print('sum of the peq is:', np.sum(peq))
-
-    #calculate the free energy
+    peq = evectors[:, index[-1]].T #normalize the eigenvector
+    peq = peq / np.sum(peq)
     peq = peq.real
-    F = -kT * np.log(peq+1e-16) #add a small number to avoid log(0)) # + 1e-16
+
+    F = -kT * np.log(peq) #add a small number to avoid log(0)) # + 1e-16
 
     return [peq, F, evectors, evalues, evalues_sorted, index]
 
 
-def try_and_optim(K, num_gaussian=10, start_state=0, end_state=0):
+def try_and_optim(K=None, num_gaussian=10, start_state=0, end_state=0):
    #print("inside try_and_optim")
     """
     here we try different gaussian params 1000 times
@@ -122,25 +120,33 @@ def try_and_optim(K, num_gaussian=10, start_state=0, end_state=0):
     
     returns the best gaussian params
     """
-    best_mfpt = 1000000000000 #initialise the best mfpt np.inf
+
+    #we construct K from loading the fes in npy files.
+    fes = np.load("Unbiased_Profile.npy")
+    K = create_K_1D(fes, N=150+1, kT=0.5981)
+
+    best_mfpt = 1e12 #initialise the best mfpt np.inf
     for i in range(1000): 
         #print("in for loop")
         rng = np.random.default_rng()
         #we set a to be 1
         a = np.ones(num_gaussian)
-        b = rng.uniform(0, 9, num_gaussian) #min/max of preloaded NaCl fes x-axis.
+        b = rng.uniform(1, 9, num_gaussian) #min/max of preloaded NaCl fes x-axis.
         c = rng.uniform(1, 5.0, num_gaussian) 
         
-        total_bias = np.zeros(50) # we were using first 50 points of the fes
+        qspace = np.linspace(2.0, 9, 150+1)
+        total_bias = np.zeros_like(qspace)
         for j in range(num_gaussian):
             #print("adding params")
-            total_bias += gaussian(np.arange(50), a[j], b[j], c[j])
+            total_bias += gaussian(qspace, a[j], b[j], c[j])
         
         K_biased = bias_K_1D(K, total_bias, kT=0.5981)
         peq = compute_free_energy(K_biased, kT=0.5981)[0]
 
         mfpts_biased = mfpt_calc(peq, K_biased)
         mfpt_biased = mfpts_biased[start_state, end_state]
+        print(sum(peq))
+        kemeny_constant_check(K.shape[0], mfpts_biased, peq)
         #print("random try:", i, "mfpt:", mfpt_biased)
         if best_mfpt > mfpt_biased:
             best_mfpt = mfpt_biased
@@ -166,7 +172,7 @@ def try_and_optim(K, num_gaussian=10, start_state=0, end_state=0):
                    best_params, 
                    args=(K,), 
                    method='Nelder-Mead', 
-                   bounds= [(0, 1)]*10 + [(0, 9)]*10 + [(1.0, 5.0)]*10, #add bounds to the parameters
+                   bounds= [(0, 1)]*10 + [(1, 9)]*10 + [(1.0, 5.0)]*10, #add bounds to the parameters
                    tol=1e-1)
 
     return res.x, best_params
@@ -185,14 +191,21 @@ def bias_M_1D(M, total_bias, kT=0.5981):
     for i in range(N):
         for j in range(N):
             u_ij = total_bias[j] - total_bias[i]
-            M_biased[i, j] = M[i, j] * np.exp(u_ij / kT)
+            M_biased[i, j] = M[i, j] * np.exp(-u_ij / 2*kT)
         M_biased[i, i] = M[i,i]
 
-    for i in range(N):
+    """for i in range(N):
         if np.sum(M_biased[:, i]) != 0:
             M_biased[:, i] = M_biased[:, i] / np.sum(M_biased[:, i])
         else:
-            M_biased[:, i] = 0
+            M_biased[:, i] = 0"""
+    #M_biased = M_biased/(np.sum(M_biased, axis=1)[:,None])# + 1e-15)
+    for i in range(M_biased.shape[0]):
+        row_sum = np.sum(M_biased[i, :])
+        if row_sum > 0:
+            M_biased[i, :] = M_biased[i, :] / row_sum
+        else:
+            M_biased[i, :] = 0
     return M_biased.real
 
 #below Markov_mfpt_calc is provided by Sam M.
@@ -238,64 +251,78 @@ def try_and_optim_M(M, working_indices, num_gaussian=10, start_state=0, end_stat
     #end_state_working_index = np.where(working_indices == end_state)[0][0] #convert end_state to the offset index space.
     start_state_working_index = np.argmin(np.abs(working_indices - start_state))
     end_state_working_index = np.argmin(np.abs(working_indices - end_state))
+    print("optimizing to get g_param from start state:", start_state_working_index, "to end state:", end_state_working_index, "in working indices.")
     
     #now our M/working_indices could be incontinues. #N = M.shape[0]
-    qspace = np.linspace(2.4, 9, 150+1) #hard coded for now.
-    best_mfpt = 1000000000000 #initialise the best mfpt np.inf
-
+    qspace = np.linspace(2.0, 9, 150+1) #hard coded for now.
+    best_mfpt = 1e12 #initialise the best mfpt np.inf
 
     for i in range(1000): 
         rng = np.random.default_rng()
         #we set a to be 1
-        a = np.ones(num_gaussian)
+        a = np.ones(num_gaussian) * 1.5
         b = rng.uniform(0, 7, num_gaussian) #fix this so it place gaussian at the working indices. it has to be in angstrom. because we need return these.
         #b = rng.uniform(b_min, b_max, num_gaussian)
-        c = rng.uniform(0.3, 1, num_gaussian) 
+        c = rng.uniform(0.3, 0.7, num_gaussian) 
         
         #we convert the working_indices to the qspace.
 
-        total_bias = np.zeros_like(qspace[working_indices])
+        total_bias = np.zeros_like(qspace)
         for j in range(num_gaussian):
-            total_bias += gaussian(qspace[working_indices], a[j], b[j], c[j])
-        
+            total_bias += gaussian(qspace, a[j], b[j], c[j])
+
+        #now we need to convert the total_bias to the working_indices space.
+        total_bias = total_bias[working_indices]
         
         M_biased = bias_M_1D(M, total_bias, kT=0.5981)
         [peq, F, evectors, evalues, evalues_sorted, index] = compute_free_energy(M_biased.T, kT=0.5981)
         
-        #test. plot F.
-        #get x-axis via qspace and working indices.
-        #x = qspace[working_indices]
-        
-        """plt.plot(x, F)
-        plt.plot(qspace[working_indices], total_bias)
-        unb_bins, unb_profile = np.load("Unbiased_Profile.npy")
-        plt.plot(unb_bins, unb_profile, label="unbiased F")
-        plt.show()"""
-        
         mfpts_biased = Markov_mfpt_calc(peq, M_biased)
         mfpt_biased = mfpts_biased[start_state_working_index, end_state_working_index]
-
+        #print(peq)
+        #kemeny_constant_check(M.shape[0], mfpts_biased, peq)
         if i % 100 == 0:
             print("random try:", i, "mfpt:", mfpt_biased)
-
+            #kemeny_constant_check(M.shape[0], mfpts_biased, peq)
+            #we plot the F.
             
         if best_mfpt > mfpt_biased:
             best_mfpt = mfpt_biased
             best_params = np.concatenate((a, b, c)) #we concatenate the params into a single array. in shape (30,)
 
-            
     print("best mfpt:", best_mfpt)
+    if False: 
+        total_bias = np.zeros_like(qspace)
+        for j in range(num_gaussian):
+            total_bias += gaussian(qspace, best_params[j], best_params[j+num_gaussian], best_params[j+2*num_gaussian])
+        working_bias = total_bias[working_indices]
+        x = qspace[working_indices]
+        plt.plot(qspace[working_indices], working_bias, label="working bias")
+        unb_bins, unb_profile = np.load("Unbiased_Profile.npy")
+        #we take first quarter.
+        unb_bins = unb_bins[:len(unb_bins)//4]
+        unb_profile = unb_profile[:len(unb_profile)//4]
+        plt.plot(unb_bins, unb_profile, label="unbiased F")
+        #plot the total_bias
+        plt.plot(qspace, total_bias, label="total bias", alpha=0.3)
+        plt.xlim(2.0, 9)
+        plt.legend()
+        plt.savefig(f"./bias_plots_fes_best_param_prop{i}.png")
+        plt.close()
     
     #now we use the best params to local optimise the gaussian params
 
     def mfpt_helper(params, M, start_state = start_state, end_state = end_state, kT=0.5981, working_indices=working_indices):
-        a = params[:num_gaussian] * 2
+        a = params[:num_gaussian]
         b = params[num_gaussian:2*num_gaussian]
         c = params[2*num_gaussian:]
-        total_bias = np.zeros_like(qspace[working_indices])
+        total_bias = np.zeros_like(qspace)
         for j in range(num_gaussian):
-            total_bias += gaussian(qspace[working_indices], a[j], b[j], c[j])
-        
+            total_bias += gaussian(qspace, a[j], b[j], c[j])
+
+        #now we need to convert the total_bias to the working_indices space.
+        total_bias = total_bias[working_indices]
+
         M_biased = bias_M_1D(M, total_bias, kT=0.5981)
         [peq, F, evectors, evalues, evalues_sorted, index] = compute_free_energy(M_biased.T, kT=0.5981)
         mfpts_biased = Markov_mfpt_calc(peq, M_biased)
@@ -310,12 +337,10 @@ def try_and_optim_M(M, working_indices, num_gaussian=10, start_state=0, end_stat
                          end_state_working_index,
                          working_indices), 
                    method='Nelder-Mead', 
-                   bounds= [(0.3, 5)]*10 + [(0,7)]*10 + [(0.3, 1)]*10, #add bounds to the parameters
+                   bounds= [(0.3, 4.5)]*10 + [(0,7)]*10 + [(0.3, 1)]*10, #add bounds to the parameters
                    tol=1e-3)
 
     if plot:
-
-        #unpack the res.x
         a = res.x[:num_gaussian]
         b = res.x[num_gaussian:2*num_gaussian]
         c = res.x[2*num_gaussian:]
@@ -331,14 +356,15 @@ def try_and_optim_M(M, working_indices, num_gaussian=10, start_state=0, end_stat
         for j in range(num_gaussian):
             total_bias_A += gaussian(qspace, a[j], b[j], c[j])
 
-    #    plt.title("Scipy optimized F with ground truth")
-    #    plt.plot(qspace[working_indices], F, label = "reconstructed biased M FES")
-    #    plt.plot(qspace, total_bias_A, label="total bias in A space.", linestyle="--", linewidth=0.5)
-    #    plt.plot(qspace[working_indices], total_bias, label="total bias in working indices", linewidth=1)
-    #    unb_bins, unb_profile = np.load("Unbiased_Profile.npy")
-    #    plt.plot(unb_bins, unb_profile, label="unbiased FES (ground truth)")
-    #    plt.plot(qspace[working_indices[start_state_working_index]], F[start_state_working_index], "o", label="start state")
-    #    plt.plot(qspace[working_indices[end_state_working_index]], F[end_state_working_index], "x", label="end state")
-    #    plt.legend()
-    #    plt.show()
+        plt.title("Scipy optimized F with ground truth")
+        plt.plot(qspace[working_indices], F, label = "reconstructed biased M FES")
+        plt.plot(qspace, total_bias_A, label="total bias in A space.", linestyle="--", linewidth=0.5)
+        plt.plot(qspace[working_indices], total_bias, label="total bias in working indices", linewidth=1)
+        unb_bins, unb_profile = np.load("Unbiased_Profile.npy")
+        plt.plot(unb_bins, unb_profile, label="unbiased FES (ground truth)")
+        plt.plot(qspace[working_indices[start_state_working_index]], F[start_state_working_index], "o", label="start state")
+        plt.plot(qspace[working_indices[end_state_working_index]], F[end_state_working_index], "x", label="end state")
+        plt.legend()
+        plt.show()
+
     return res.x    #, best_params